adcd1a87d1d0aa2bce6b7db2d632d85b6b036893
Daniel Amarante <daniel.amarante2@gmail.com>
2018-01-30 17:03:09 -0200

    [#00] Configure cors

--- config/config.exs
+++ config/config.exs
@@ -6,22 +6,26 @@
 use Mix.Config
 
 # General application configuration
-config :code_clicker,
-  ecto_repos: [CodeClicker.Repo]
+config :code_clicker, ecto_repos: [CodeClicker.Repo]
 
 # Configures the endpoint
 config :code_clicker, CodeClickerWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "QHOVPLLoKxQNiQBAOo9QEj3yhw9Y+GBFPkyUfto0ldRbtyBFAGTrzi1ZBL+yqf0f",
   render_errors: [view: CodeClickerWeb.ErrorView, accepts: ~w(json)],
-  pubsub: [name: CodeClicker.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: CodeClicker.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+# Configures CORS
+config :cors_plug,
+  origin: ["*"],
+  max_age: 86_400,
+  methods: ["GET", "POST", "PATCH", "DELETE"]
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
