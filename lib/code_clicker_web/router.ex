5ae515c87943c431cbb813382087aa3d6fe1d3ab
Daniel Amarante <daniel.amarante2@gmail.com>
2018-01-31 09:33:17 -0200

    [#00] Remove unused functions

--- lib/code_clicker_web/router.ex
+++ lib/code_clicker_web/router.ex
@@ -7,6 +7,6 @@ defmodule CodeClickerWeb.Router do
 
   scope "/api", CodeClickerWeb do
     pipe_through(:api)
-    resources("/items", ItemController, except: [:new, :edit])
+    resources("/items", ItemController, only: [:index, :show])
   end
 end
