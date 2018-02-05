2e7c5414156542f7df7268e144c0b06d5f6b522f
Daniel Amarante <daniel.amarante2@gmail.com>
2018-02-05 12:57:09 -0200

    [#00] Fix guardian class

--- lib/code_clicker/accounts/guardian.ex
+++ lib/code_clicker/accounts/guardian.ex
@@ -2,7 +2,7 @@ defmodule CodeClicker.Accounts.Guardian do
   use Guardian, otp_app: :code_clicker
 
   def subject_for_token(user, _claims) do
-    {ok, to_string(user.id)}
+    {:ok, to_string(user.id)}
   end
 
   def resource_from_claims(claims) do
