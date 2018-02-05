59de9c6a0625b424aacf0b415c47105d1f655af3
Daniel Amarante <daniel.amarante2@gmail.com>
2018-02-05 09:38:17 -0200

    [#00] Add basic configuration to guardian

--- config/config.exs
+++ config/config.exs
@@ -26,6 +26,13 @@ config :cors_plug,
   max_age: 86_400,
   methods: ["GET", "POST", "PATCH", "DELETE"]
 
+# Configure Guardian
+
+config :code_clicker, MyApp.Guardian,
+  issuer: "CodeClicker",
+  # mix guardian.gen.secret
+  secret_key: "PvZ3uYsAn4IPnHJbeoj2BPyQADyyhqISkp/rGD5NhE7YYHxN1/7B1ziJ8Pl3iWb4"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
