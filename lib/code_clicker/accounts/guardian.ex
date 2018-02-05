5383de517b39fe6f4f13d45df139b435ce643148
Daniel Amarante <daniel.amarante2@gmail.com>
2018-02-05 12:52:37 -0200

    [#00] Create guardian file

---
+++ lib/code_clicker/accounts/guardian.ex
@@ -0,0 +1,13 @@
+defmodule CodeClicker.Accounts.Guardian do
+  use Guardian, otp_app: :code_clicker
+
+  def subject_for_token(user, _claims) do
+    {ok, to_string(user.id)}
+  end
+
+  def resource_from_claims(claims) do
+    user_id = claims["sub"]
+    user = Accounts.get_user(user_id)
+    {:ok, user}
+  end
+end
